f025c5ee61d21bff88dfab222f86985c0853dca0
lucas-leoni <lucas.leoni09@gmail.com>
2022-05-12 21:55:42 -0300

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container d-flex justify-content-center">
+    <div
+      v-for="(item, index) in user"
+      :key="index"
+      class="card"
+      style="width: 18rem"
+    >
+      <img :src="item.picture.medium" class="card-img-top" />
+      <div class="card-body">
+        <h5 class="card-title">{{ item.name.first }} {{ item.name.last }}</h5>
+        <h6 class="card-title">{{ item.email }}</h6>
+        <h6 class="card-title">Age: {{ item.dob.age }}</h6>
+        <h6 class="card-title">Country {{ item.location.country }}</h6>
+        <button @click="getOtherUser" class="btn btn-primary">
+          Other User
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    user: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    getOtherUser() {
+      this.$emit("onChangeUser");
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,87 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello! My Status is {{ status }}</h1>
+    <hr />
+
+    <button @click="getUser">Clique {{ count }}</button>
+
+    <div>
+      <h2>{{ user.login }}</h2>
+      <p v-if="status">
+        <strong>Número de Repositórios |</strong> {{ user.public_repos }}
+      </p>
+      <span>{{ user.html_url }}</span>
+      <p>{{ user.bio }}</p>
+      <img :src="user.avatar_url" alt="" />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  data() {
+    return {
+      status: false,
+      count: 0,
+      user: {},
+    };
+  },
+  /* beforeCreate() {
+    console.log("hook - before-create - is done?", this.status);
+  },
+  created() {
+    // vamos utilizar sempre
+    console.log("pre-created", this.status);
+    this.status = true;
+    console.log("created", this.status);
+    console.log("DOM CREATED -", this.$el);
+  },
+  beforeMount() {
+    console.log("BEFORE MOUNT - ", this.status);
+    console.log("DOM BEFORE MOUNT -", this.$el);
+  },
+  mounted() {
+    // vamos utilizar sempre
+    console.log("MOUNTED - ", this.status);
+    console.log("DOM MOUNTED -", this.$el);
+  }, */
+  /* beforeUpdate() {
+    console.log("beforeUpdate - ", this.status);
+    console.log("DOM beforeUpdate -", this.$el);
+  },
+  updated() {
+    console.log("updated - ", this.status);
+    console.log("DOM updated -", this.$el);
+  }, */
+
+  mounted() {
+    this.getUser();
+  },
+
+  methods: {
+    contador() {
+      this.count++;
+    },
+    getUser() {
+      const url = "https://api.github.com/users/lucas-leoni";
+      axios
+        .get(url)
+        .then((response) => {
+          console.log(response.data);
+          this.user = response.data;
+          this.status = true;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      // duas formas de fazer
+      // fetchApi -> ES6
+      // axios -> Lib
+    },
+  },
+};
 </script>
